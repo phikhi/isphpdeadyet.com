7e2baaa2e43581c7cf1ce94ef104007b93565e3b
Philippe Khill <hello@phikhi.com>
2023-08-23 11:55:49 +0200

    updating homepage template

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     @vite('resources/css/app.css')
 </head>
-<body>
+<body class="h-full">
     {{ $slot }}
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
-    <h1>NO</h1>
+    <div class="min-h-full flex items-center justify-center">
+        <div class="bg-white px-12 py-8 rounded-lg shadow-lg">
+            <h1 class="text-7xl font-semibold">Not yet!</h1>
+        </div>
+    </div>
 </x-layout>
