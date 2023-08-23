b72830875cc9e85d24b907ee32b331b0fd27b31b
Philippe Khill <hello@phikhi.com>
2023-08-23 11:49:30 +0200

    adding tailwindcss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    @vite('resources/css/app.css')
 </head>
 <body>
     {{ $slot }}
